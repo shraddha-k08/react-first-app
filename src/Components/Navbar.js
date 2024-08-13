36bf9597cb9dc2aee89dce43f94cea3dbf76cba2
shraddha kulkarni <rgkulkarni567@gmail.com>
2024-08-13 14:07:48 +0530

    Adding routes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-//import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-sm navbar-${props.mode} bg-${props.mode}`}>
